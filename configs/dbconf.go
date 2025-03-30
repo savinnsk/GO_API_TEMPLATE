93b3290e065cced071aedc75c477c9f98afb53c3
Savio Picanço <saviopbrito@outlook.com>
2025-03-30 18:01:43 -0300

    feat: add a func to run transactions and rollback database

--- configs/dbconf.go
+++ configs/dbconf.go
@@ -1,18 +1,26 @@
 package configs
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/savinnsk/api-template-go/internal/infra/sqlc"
 )
 
-var dbConn *sql.DB 
+
+var dbConn *sql.DB
+type DbInstance struct { 
+	db *sql.DB
+	*sqlc.Queries
+	} 
 
 func InitDb(cfg *conf) *sql.DB {
 	dns := fmt.Sprintf("%s:%s@tcp(%s)/%s",cfg.DBUser,cfg.DBPassword,cfg.DBHost,cfg.DBDatabase)
 	var err error
-	dbConn, err = sql.Open("mysql",dns)
+	
+	dbConn, err := sql.Open("mysql",dns)
 	if  err != nil {
 		panic(err)
 	}
@@ -20,9 +28,33 @@ func InitDb(cfg *conf) *sql.DB {
 	return dbConn
 }
 
-func GetInstanceDB() *sql.DB {
+func GetInstanceDB() *DbInstance {
 	if dbConn == nil {
 		log.Fatal("Database instance error.")
 	}
-	return dbConn
+	return &DbInstance{
+		db : dbConn,
+		Queries: sqlc.New(dbConn),
+	}
 }
+
+func (dbInst *DbInstance) CallTx(ctx context.Context, fn func(*sqlc.Queries) error) error {
+	tx, err := dbInst.db.BeginTx(ctx,nil)
+	if err != nil {
+		return err
+	}
+
+	q := sqlc.New(tx)
+	err = fn(q)
+	if err != nil {
+		if errRb := tx.Rollback(); errRb != nil {
+			 log.Printf("error on rollback: %v, original error: %v", errRb,err)
+			 return fmt.Errorf("error on rollback: %v, original error: %v", errRb,err)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+
